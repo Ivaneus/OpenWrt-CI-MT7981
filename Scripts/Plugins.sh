2848327e2f9880c999e445e8a5d4c0214590e87f
Ivaneus <908221936@qq.com>
2023-10-09 16:51:38 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -19,14 +19,19 @@ git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-con
 
 #lucky
 #git clone --depth=1 --single-branch --branch "main" https://github.com/gdy666/luci-app-lucky.git
+git clone https://github.com/kenzok8/small.git
+git clone https://github.com/immortalwrt/homeproxy.git
+git clone https://github.com/vernesong/OpenClash.git
+git clone https://github.com/daeuniverse/daed.git
+git clone https://github.com/QiuSimons/luci-app-daed.git
 svn export https://github.com/v2rayA/v2raya-openwrt/branches/master/luci-app-v2raya
+
 #svn export https://github.com/v2rayA/v2raya-openwrt/branches/master/v2raya
-svn export https://github.com/kiddin9/openwrt-packages/branches/master/luci-app-homeproxy
-svn export https://github.com/kiddin9/openwrt-packages/branches/master/luci-app-passwall2
-svn export https://github.com/kiddin9/openwrt-packages/branches/master/luci-app-bypass
-svn export https://github.com/kiddin9/openwrt-packages/branches/master/luci-app-ssr-plus
-svn export https://github.com/kiddin9/openwrt-packages/branches/master/luci-app-vssr
-svn export https://github.com/kiddin9/openwrt-packages/branches/master/luci-app-openclash
-svn export https://github.com/kiddin9/openwrt-packages/branches/master/firewall4
-svn export https://github.com/kiddin9/openwrt-packages/branches/master/daed
-svn export https://github.com/kiddin9/openwrt-packages/branches/master/luci-app-daed
+#svn export https://github.com/kiddin9/openwrt-packages/branches/master/luci-app-passwall2
+#svn export https://github.com/kenzok8/small/branches/master/luci-app-bypass
+#svn export https://github.com/kiddin9/openwrt-packages/branches/master/luci-app-ssr-plus
+#svn export https://github.com/kiddin9/openwrt-packages/branches/master/luci-app-vssr
+#svn export https://github.com/kiddin9/openwrt-packages/branches/master/luci-app-openclash
+#svn export https://github.com/kiddin9/openwrt-packages/branches/master/firewall4
+#svn export https://github.com/kiddin9/openwrt-packages/branches/master/daed
+#svn export https://github.com/kiddin9/openwrt-packages/branches/master/luci-app-daed

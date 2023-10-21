b2094da7a4f0876baf623a087e45d03fb5f711e9
Ivaneus <908221936@qq.com>
2023-10-21 11:39:02 +0800

    Create Pluginstest.sh

---
+++ Scripts/Pluginstest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#Design Theme
+git clone --depth=1 --single-branch --branch $(echo $OpenWrt_URL | grep -iEq "lede|padavanonly" && echo "main" || echo "js") https://github.com/papagaye744/luci-theme-design.git
+git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
+
+#Argon Theme
+#git clone --depth=1 --single-branch --branch $(echo $OpenWrt_URL | grep -iEq "lede|padavanonly" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
+#git clone --depth=1 --single-branch --branch $(echo $OpenWrt_URL | grep -iEq "lede|padavanonly" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
+
+#Extra APP
+git clone --depth=1 --single-branch https://github.com/kiddin9/openwrt-packages
+
+
+
+
+
+
+
+

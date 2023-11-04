4bcba97fdca6202ab9ff41d5c376b417702074ef
Ivaneus <908221936@qq.com>
2023-11-04 13:13:02 +0800

    Update Pluginstest.sh

--- Scripts/Pluginstest.sh
+++ Scripts/Pluginstest.sh
@@ -9,13 +9,13 @@ git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-con
 #git clone --depth=1 --single-branch --branch $(echo $OpenWrt_URL | grep -iEq "lede|padavanonly" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
 
 #Extra APP
-git clone --depth=1 --single-branch https://github.com/kiddin9/openwrt-packages
+git clone https://github.com/kiddin9/openwrt-packages
 git clone https://github.com/xiaorouji/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall2
 #v2raya
 svn export https://github.com/v2rayA/v2raya-openwrt/branches/master/luci-app-v2raya
 svn export https://github.com/v2rayA/v2raya-openwrt/branches/master/v2raya
-rm -rf ./openwrt-packages/luci-base ./openwrt-packages/firewall ./openwrt-packages/firewall ./openwrt-packages/luci-app-upnp ./openwrt-packages/miniupnpd-iptables ./openwrt-packages/luci-app-zerotier ./openwrt-packages/luci-app-ddns ./openwrt-packages/luci-app-passwall ./openwrt-packages/luci-app-passwall2 ./openwrt-packages/luci-app-v2raya ./openwrt-packages/v2raya
+rm -rf ./openwrt-packages/luci-base ./openwrt-packages/firewall ./openwrt-packages/luci-app-upnp ./openwrt-packages/miniupnpd-iptables ./openwrt-packages/luci-app-zerotier ./openwrt-packages/luci-app-ddns ./openwrt-packages/luci-app-passwall ./openwrt-packages/luci-app-passwall2 ./openwrt-packages/luci-app-v2raya ./openwrt-packages/v2raya
 
 
 
